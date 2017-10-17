32af30da474255fdf38918cfffbdf1edb830724a
Migal Lewis <migal.lewis@standardbank.co.za>
2017-10-17 22:15:23 +0200

    initial ,Added header and added manage components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ManageComponent } from './manage/manage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule
