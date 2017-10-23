e071a8a46e2131886ad6028eda1fe2ad7222a124
Migal Lewis <migal.lewis@standardbank.co.za>
2017-10-23 09:33:38 +0200

    Added contact types

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Profile, Contact, CONTACT_TYPE, PersonOfContact } from './profile.model';
+import { Profile, Contact, CONTACT_TYPE, PersonOfContact, Location } from './profile.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -14,9 +14,14 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit() {
     this.profile=new Profile("Farmers brothers","","https://upload.wikimedia.org/wikipedia/en/thumb/7/78/Farmer_Brothers_Company_Logo.png/200px-Farmer_Brothers_Company_Logo.png",
-    "We a farmers brothers are farmers and brothers","we have alot",4,[],[
+    "We a farmers brothers are farmers and brothers","we have alot",4,[
+      new Location("Head office","456 wayne manor, Gotham ,2468",true),
+      new Location("Farm","456 wayne penthouse, Gotham ,2468",false)
+    ],[
       new PersonOfContact("Bruce Wayne","Bruce wayne is the owner",[new Contact("main phone",CONTACT_TYPE.PHONE,"735-185-7301"),
-      new Contact("Bruce wayne is the owner",CONTACT_TYPE.EMAIL,"brucewayne@gmail.com")]),
+      new Contact("Bruce wayne is the owner",CONTACT_TYPE.EMAIL,"brucewayne@gmail.com"),
+      new Contact("Bruce wayne is the owner",CONTACT_TYPE.WHATSAPP,"735-185-7301")
+    ]),
       new PersonOfContact("Bruce Wayne","Bruce wayne is the owner",[new Contact("main phone",CONTACT_TYPE.PHONE,"735-185-7301"),
       new Contact("Bruce wayne is the owner",CONTACT_TYPE.EMAIL,"brucewayne@gmail.com")]),
       new PersonOfContact("Farmers brothers General","The company general",[new Contact("Our website",CONTACT_TYPE.WEBSITE,"www.farmersBrothers.com"),
@@ -24,7 +29,7 @@ export class ProfileComponent implements OnInit {
       new Contact("Facebook page",CONTACT_TYPE.FACEBOOK,"www.facebook.com/farmersBrothers"),
       new Contact("Linkden page",CONTACT_TYPE.LINKDEN,"www.linkden.com/farmersBrothers")
     ])
-    ],[]);
+    ]);
   }
 
 }
--- src/app/profile/profile.model.ts
+++ src/app/profile/profile.model.ts
@@ -1,18 +1,19 @@
+
 // surname 
 // photo
 // location
 // description
 // experience
 // Rating
-export class Location{
-    public name : string;
-    public address : string;
-    public main : boolean;
+export class Location {
+    public name: string;
+    public address: string;
+    public main: boolean;
 
-    constructor(name :string,address :string,main :boolean){
-        this.name=name;
-        this.address=address;
-        this.main=main;
+    constructor(name: string, address: string, main: boolean) {
+        this.name = name;
+        this.address = address;
+        this.main = main;
     }
 }
 
@@ -24,7 +25,8 @@ export enum CONTACT_TYPE {
     FACEBOOK,
     TWITTER,
     INSTAGRAM,
-    LINKDEN
+    LINKDEN,
+    WHATSAPP
 }
 export enum SOCIAL_MEDIA_TYPE {
     YOUTUBE,
@@ -34,64 +36,93 @@ export enum SOCIAL_MEDIA_TYPE {
     LINKDEN
 }
 export class SocialMedia {
-    public type :SOCIAL_MEDIA_TYPE;
-    public link :string;
+    public type: SOCIAL_MEDIA_TYPE;
+    public link: string;
 }
-export class PersonOfContact{
-    public name : string;
-    public roleDescription : string;
-    public contacts : Contact[];
+export class PersonOfContact {
+    public name: string;
+    public roleDescription: string;
+    public contacts: Contact[];
 
-    constructor(name :string,roleDescription :string,contacts :Contact[]){
-        this.name=name;
-        this.roleDescription=roleDescription;
-        this.contacts=contacts;
+    constructor(name: string, roleDescription: string, contacts: Contact[]) {
+        this.name = name;
+        this.roleDescription = roleDescription;
+        this.contacts = contacts;
     }
 
-}
-
-export class Contact{
-    public description : string;
-    public contactType :CONTACT_TYPE;
-    public value : string; 
-
-    constructor(description :string,contactType :CONTACT_TYPE,value:string){
-        this.description=description;
-        this.contactType=contactType;
-        this.value=value;
-    }
 }
 
-export class Profile{
-    public name :string;
-    public profileImage :string;
-    public logo :string;
-    public description : string;
-    public experiance : string;
-    public rating : number;
-    public locations : Location[];
-    public contacts : PersonOfContact[];
-    public socialMedia : SocialMedia[];
+export class Contact {
+    public description: string;
+    public contactType: CONTACT_TYPE;
+    public value: string;
 
-    constructor(name :string,profileImage :string,logo :string,
-        description : string,experiance : string,rating : number,
-        locations : Location[],contacts : PersonOfContact[],socialMedia : SocialMedia[]){
-            this.name =name;
-            this.profileImage =profileImage;
-            this.logo =logo;
-            this.description = description;
-            this.experiance = experiance;
-            this.rating = rating;
-            this.locations = locations;
-            this.contacts = contacts;
-            this.socialMedia = socialMedia;
+    constructor(description: string, contactType: CONTACT_TYPE, value: string) {
+        this.description = description;
+        this.contactType = contactType;
+        this.value = value;
+    }
+}
+
+export class Profile {
+    public name: string;
+    public profileImage: string;
+    public logo: string;
+    public description: string;
+    public experiance: string;
+    public rating: number;
+    public locations: Location[];
+    public contacts: PersonOfContact[];
+
+    constructor(name: string, profileImage: string, logo: string,
+        description: string, experiance: string, rating: number,
+        locations: Location[], contacts: PersonOfContact[]) {
+        this.name = name;
+        this.profileImage = profileImage;
+        this.logo = logo;
+        this.description = description;
+        this.experiance = experiance;
+        this.rating = rating;
+        this.locations = locations;
+        this.contacts = contacts;
     }
 
-    getIcon(){
-        return "assets/phone.png";
+    getIcon(contactType: CONTACT_TYPE) {
+        var image: string;
+        switch (contactType) {
+            case CONTACT_TYPE.EMAIL:
+                image = "assets/email.png";
+                break;
+            case CONTACT_TYPE.PHONE:
+                image = "assets/phone.png";
+                break;
+                case CONTACT_TYPE.YOUTUBE:
+                image = "assets/youtube.png";
+                break;
+                case CONTACT_TYPE.LINKDEN:
+                image = "assets/linkden.png";
+                break;
+                case CONTACT_TYPE.TWITTER:
+                image = "assets/twitter.png";
+                break;
+                case CONTACT_TYPE.WEBSITE:
+                image = "assets/website.png";
+                break;
+                case CONTACT_TYPE.WHATSAPP:
+                image = "assets/whatsapp.png";
+                break;
+                case CONTACT_TYPE.FACEBOOK:
+                image = "assets/facebook.png";
+                break;
+                case CONTACT_TYPE.INSTAGRAM:
+                image = "assets/instagram.png";
+                break;
+            default: image="assets/generalContact.png"
+        }
+        return image;
     }
 
-    getContactTypeString(){
+    getContactTypeString() {
         return "Phone";
     }
 
